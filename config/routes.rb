4ffa37f187ad29dafcc6417f0bc6b3af54bfe89b
maurixavi <mxavierarevalo@gmail.com>
2023-03-07 18:05:45 -0300

    Update root and views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :friends
   #get 'home/index'
   get 'home/about'
-  root 'friends#index'
+  root 'films#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
